c0afa62e3fcbfdf9a8b9c94f8dd353eeb9af7020
Deepaksingh126 <lalmohansingh@gmail.com>
2025-05-13 16:52:59 +0530

    add react functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <header>
-      <h1>Deepak.dev</h1>
-      <nav>
+    <header className="w-full flex items-center justify-around pt-5 mb-12">
+      <h1 className="text-3xl font-bold">Deepak.dev</h1>
+      <nav className="flex gap-8">
 
+        <NavLink className={({ isActive }) =>
+          `${isActive ? "bg-sky-500/40" : ""} text-white font-bold py-1 px-3 rounded-xl hover:bg-sky-500/40`
+        } to="/">home</NavLink>
 
-        <Link to="/">home</Link>
-        <Link to="/project">projects</Link>
-        <Link to="/blogs">blogs</Link>
+        <NavLink className={({ isActive }) =>
+          `${isActive ? "bg-sky-500/40" : ""} text-white font-bold py-1 px-3 rounded-xl hover:bg-sky-500/40`
+        } to="/project">projects</NavLink>
+
+        <NavLink className={({ isActive }) =>
+          `${isActive ? "bg-sky-500/40" : ""} text-white font-bold py-1 px-3 rounded-xl hover:bg-sky-500/40`
+        } to="/blogs">blogs</NavLink>
+        
       </nav>
     </header>
   );
